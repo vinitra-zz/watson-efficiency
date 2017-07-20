562f4d320eea4a666025e39536c99115102db98c
Antara Palit <Antara.Palit@ibm.com>
2017-07-20 19:28:34 -0400

    finished speechtotext

--- speechtotext.py
+++ speechtotext.py
@@ -1,4 +1,5 @@
 import json
+from os.path import join, dirname
 from watson_developer_cloud import SpeechToTextV1
 
 speech_to_text = SpeechToTextV1(
@@ -11,7 +12,7 @@ print(json.dumps(speech_to_text.models(), indent=2))
 
 print(json.dumps(speech_to_text.get_model('en-US_BroadbandModel'), indent=2))
 
-with open(join(dirname(__file__), 'Downloads/911_Call.wav'),
+with open(join(dirname(__file__), '/Users/antara.palit@ibm.com/Downloads/911_Call.wav'),
           'rb') as audio_file:
     print(json.dumps(speech_to_text.recognize(
         audio_file, content_type='audio/wav', timestamps=True,
