9698282e28a01f0ca64e9371616cf144f2a8c1db
vinitra.swamy <vinitra.swamy@ibm.com>
2017-07-20 17:14:59 -0400

    working tonal Analysis with emotion arc

--- model/tonalAnalysis.py
+++ model/tonalAnalysis.py
@@ -1,6 +1,9 @@
 import json
 from watson_developer_cloud import ToneAnalyzerV3
 import transcriptManipulation
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import spline
 
 toneAnalyzer = ToneAnalyzerV3(
   version='2016-05-19',
@@ -15,4 +18,24 @@ caller_text = transcriptManipulation.stripCallerText(data)
 toneJson = json.dumps(toneAnalyzer.tone(text=caller_text), indent=2)
 
 arrayData = json.loads(toneJson)
-print(toneJson)
+
+emotional_range_scores = np.array([sentence.get('tone_categories')[2].get('tones')[4].get('score')  for sentence in arrayData.get('sentences_tone') if sentence.get('tone_categories') != []]) * 100 
+anger_scores = np.array([sentence.get('tone_categories')[0].get('tones')[0].get('score')  for sentence in arrayData.get('sentences_tone') if sentence.get('tone_categories') != []]) * 100
+fear_scores = np.array([sentence.get('tone_categories')[0].get('tones')[2].get('score')  for sentence in arrayData.get('sentences_tone') if sentence.get('tone_categories') != []]) * 100
+sad_scores = np.array([sentence.get('tone_categories')[0].get('tones')[4].get('score')  for sentence in arrayData.get('sentences_tone') if sentence.get('tone_categories') != []]) * 100
+s = np.arange(len(emotional_range_scores))
+
+plt.gca().set_color_cycle(['purple', 'red', 'orange', 'blue'])
+
+plt.plot(s, emotional_range_scores)
+plt.plot(s, anger_scores)
+plt.plot(s, fear_scores)
+plt.plot(s, sad_scores)
+
+plt.legend(['Emotion Intensity', 'Anger', 'Fear', 'Sadness'], loc='upper left')
+
+plt.xlabel('Time')
+plt.ylabel('Emotion Percentages')
+plt.title('911 Caller Emotional Range')
+plt.savefig("emotionalrange.png")
+plt.show()
